7c685eeb8fbbfbc5be8f1f68e68fed12c5ac7871
AkshithShetty599 <akshithk56@gmail.com>
2025-06-30 14:12:51 +0530

    Modify main.py

--- App/main.py
+++ App/main.py
@@ -15,7 +15,7 @@ hide_streamlit_style = """
 st.markdown(hide_streamlit_style, unsafe_allow_html=True)
 
 def get_clean_data():
-    data = pd.read_csv("data/data.csv")
+    data = pd.read_csv("Data/data.csv")
     data = data.drop(['Unnamed: 32', 'id'], axis=1)
     data['diagnosis'] = data['diagnosis'].map({ 'M': 1, 'B': 0 })
     return data
